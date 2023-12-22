4895e03c1a09c2c21bcfc7b0568444610c6c38b4
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2023-12-22 20:12:07 +0530

    Update logging.py

--- AnonX/logging.py
+++ AnonX/logging.py
@@ -5,13 +5,12 @@ logging.basicConfig(
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
     datefmt="%d-%b-%y %H:%M:%S",
     handlers=[
-        RotatingFileHandler(
-            LOG_FILE_NAME, maxBytes=5000000, backupCount=10
-        ),
+        logging.FileHandler("log.txt"),
         logging.StreamHandler(),
     ],
 )
 
+logging.getLogger("httpx").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
 
